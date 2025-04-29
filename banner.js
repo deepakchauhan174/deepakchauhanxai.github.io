a4c27ba029d9c81e1e7629764cbd33a1caa7df4f
deepakchauhan174 <deepak.chauhan.dk.community@gmail.com>
2025-04-29 10:34:34 +0530

    Added dynamic Google Sheet fetch system...
    Added dynamic Google Sheet fetch system for today's dialogue and story content"

---
+++ banner.js
@@ -0,0 +1,11 @@
+function openStory() {
+  window.location.href = "Ai_bhai_shth_hai.html";
+}
+
+// 5 मिनट बाद Banner Hide करना
+setTimeout(function() {
+  var banner = document.getElementById('promoBanner');
+  if(banner) {
+    banner.style.display = 'none';
+  }
+}, 300000); // 5 minutes = 300000 milliseconds
