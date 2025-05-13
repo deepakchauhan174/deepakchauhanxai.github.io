948068db2d1580ba6b8147d5321ee5446c4efef4
deepakchauhan174 <deepak.chauhan.dk.community@gmail.com>
2025-05-13 16:17:09 +0530

    Update box11_live_image.js

--- scripts/box11_live_image.js
+++ scripts/box11_live_image.js
@@ -1,15 +1,16 @@
-<script>
-  fetch("https://script.google.com/macros/s/AKfycbymUjv2qf6EQt00nQivEhWJLrxUld8aQKiGZbyBoqPxCkGQMyL10D-zI6IJiX9AGc-Bdw/exec")
+fetch("https://script.google.com/macros/s/AKfycbymUjv2qf6EQt00nQivEhWJLrxUld8aQKiGZbyBoqPxCkGQMyL10D-zI6IJiX9AGc-Bdw/exec")
     .then(response => response.json())
     .then(data => {
       const imageData = data.find(row => row.title === "title1");
       if (imageData && imageData.ImageURL) {
-        document.getElementById("box11-image").style.backgroundImage = `url('${imageData.ImageURL}')`;
-        document.getElementById("box11-image").style.backgroundSize = "cover";
-        document.getElementById("box11-image").style.backgroundPosition = "center";
+        const box = document.getElementById("box11-image");
+        if (box) {
+          box.style.backgroundImage = `url('${imageData.ImageURL}')`;
+          box.style.backgroundSize = "cover";
+          box.style.backgroundPosition = "center";
+        }
       }
     })
     .catch(error => {
       console.error("Box11 Image Load Error:", error);
     });
-</script>
