944cba9aa661ecd337fc0760d5b594dd185dfd00
deepakchauhan174 <deepak.chauhan.dk.community@gmail.com>
2025-06-07 08:02:55 +0530

    Create त्योहार.js

---
+++ scripts/त्योहार.js
@@ -0,0 +1,12 @@
+// 🔥 Popup Data Load from Google Sheet
+fetch('https://script.google.com/macros/s/AKfycbzVszNeD21dFuamuZNCC-d_lqmrV11y9c5nIOVHeTe3JnpWT3IBkC45Fq8iN99XEAL5/exec')
+  .then(res => res.json())
+  .then(data => {
+    document.getElementById("popupPhoto").src = data.photo;
+    document.getElementById("popupTitle").textContent = data.title;
+    document.getElementById("popupShayari").textContent = data.shayari;
+    document.getElementById("popupSignature").textContent = data.signature;
+  })
+  .catch(err => {
+    console.error("Error loading popup content", err);
+  });
